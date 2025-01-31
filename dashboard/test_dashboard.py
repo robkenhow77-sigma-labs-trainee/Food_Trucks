dbe2468302e73d1a60af7d3af88b13f4c02be555
robkenhow77-sigma-labs-trainee <trainee.robert.howarth@sigmalabs.co.uk>
2025-01-31 16:26:42 +0000

    testing

--- dashboard/test_dashboard.py
+++ dashboard/test_dashboard.py
@@ -1,22 +1,30 @@
 """Testing dashboard.py functions"""
 # Third-party imports
-from unittest.mock import MagicMock
-
+from unittest.mock import patch
 
 # Local imports
-from dashboard import get_payment_method_map
+from dashboard import get_payment_method_map, get_truck_name_map
 
+# Test the function using patching
+@patch('dashboard.query')
+def test_get_payment_method_map(mock_query):
+    mock_query.return_value = [
+        {"payment_method_id": 1, "payment_method": "cash"},
+        {"payment_method_id": 2, "payment_method": "card"}
+        ]
+       
+    expected = {"1": "cash", "2": "card"}
+    assert get_payment_method_map(None) == expected
 
-def add_1(a:int):
-    return a + 1
+    mock_query.return_value = []
+    assert get_payment_method_map(None) == {}
+    
 
-print(add_1(1))
-
-
-def test_get_payment_method_map():
-    mock = MagicMock()
-    mock[3] = 'fish'
-    mock.__setitem__.assert_called_with(3, 'fish')
-    mock.__getitem__.return_value = 'result'
-
-
+@patch('dashboard.query')
+def test_get_truck_name_map(mock_query):
+    mock_query.return_value = [
+        {"truck_id": 1, "truck_name": "a truck"},
+        {"truck_id": 2, "truck_name": "another truck"}
+    ]
+    expected = {"1": "a truck", "2": "another truck"}
+    assert get_truck_name_map(None) == expected
